a20a375f843b191186fc99487f991f2f4dbaebf2
Bergendahl94 <william_bergendahl@hotmail.com>
2014-11-06 12:43:12 +0100

    Added Healthbar

--- TweetnCrawl/Assets/Resources/Scripts/CharacterHealth.cs
+++ TweetnCrawl/Assets/Resources/Scripts/CharacterHealth.cs
@@ -4,9 +4,24 @@ using System.Collections;
 public class CharacterHealth : MonoBehaviour {
 	
 	public int health = 100;
+	public Texture2D HB;
 	public Transform Player;
+
+
+	public float x;
+	public float y;
+	public float w;
+	public float h;
+
+	public Material mat;
+
 	
-	
+	void OnGUI() {
+		if (Event.current.type.Equals(EventType.Repaint)) {
+			Rect box = new Rect(x, y, w, h);
+			Graphics.DrawTexture(box, HB, mat);
+		}
+		}
 	
 	// Use this for initialization
 	void Start () {
@@ -16,6 +31,16 @@ public class CharacterHealth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		float healthy = 1f-(health/100f);
+		if (healthy == 0) {
+			healthy = 0.1f;
+				}
+		mat.SetFloat ("_Cutoff", healthy);
+	
+
+
+
 		if (health <= 0) {
 			//no damage method is implemented yet so the enemy cannot die as of now.
 			print("Blaaah you killed me!");
