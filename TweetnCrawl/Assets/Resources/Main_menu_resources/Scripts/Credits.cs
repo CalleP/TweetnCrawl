9a4c2074421c45f68d9923a6ed127e4705d54a92
Bergendahl94 <william_bergendahl@hotmail.com>
2014-11-15 20:48:07 +0100

    Added Credits & changed camera view perspective
    Added Credits & changed camera view perspective

--- TweetnCrawl/Assets/Resources/Main_menu_resources/Scripts/Credits.cs
+++ TweetnCrawl/Assets/Resources/Main_menu_resources/Scripts/Credits.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Credits : MonoBehaviour 
 {
 	public Texture2D Image;
 	public float x;
 	public float y;
+	float speed = 20f; 
+	bool crawling = false;
+	float time;
+	public GameObject credits;
+
 	
 	void OnGUI()
 	{
 		AutoResize(1920, 1080);
 		if (GUI.Button (new Rect (x, y, Image.width, Image.height), Image)) {
-			
+			crawling = true;
 			print("Credits pressed");
 			
 		}
@@ -22,5 +28,27 @@ public class Credits : MonoBehaviour
 	{
 		Vector2 resizeRatio = new Vector2((float)Screen.width / screenWidth, (float)Screen.height / screenHeight);
 		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(resizeRatio.x, resizeRatio.y, 1.0f));
+	}
+
+
+	void Start()
+	{
+		time = Time.deltaTime;
+		credits = GameObject.Find("CreditsObject");
+		}
+
+	 void Update ()
+	{
+		if (crawling == true) {
+
+			//credits.transform.Translate(Vector3.forward * Time.deltaTime);
+			credits.transform.Translate(Vector3.forward * Time.deltaTime, Camera.main.transform);
+
+						if (credits.transform.position.z > 70) {
+								crawling = false;
+				credits.transform.position = new Vector3(-0.6568451f, -4.86139f, -7.337857f);
+
+						}
+				}
 	}
 }
