3142a1b299469bd5366975312f30cfd6fbfe04ac
Bergendahl94 <william_bergendahl@hotmail.com>
2014-11-15 21:14:34 +0100

    Added simple networkchecker & now loads game

--- TweetnCrawl/Assets/Resources/Main_menu_resources/Scripts/PlayGame.cs
+++ TweetnCrawl/Assets/Resources/Main_menu_resources/Scripts/PlayGame.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayGame : MonoBehaviour 
 {
 	public Texture2D Image;
 	public float x;
 	public float y;
+	WWW www; 
 	
 	void OnGUI()
 	{
@@ -12,6 +14,8 @@ public class PlayGame : MonoBehaviour
 		if (GUI.Button (new Rect (x, y, Image.width, Image.height), Image)) {
 
 			print("Play game pressed");
+			www = new WWW("www.google.com");
+			StartCoroutine(checkConnection());
 
 				}
 		
@@ -23,4 +27,23 @@ public class PlayGame : MonoBehaviour
 		Vector2 resizeRatio = new Vector2((float)Screen.width / screenWidth, (float)Screen.height / screenHeight);
 		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(resizeRatio.x, resizeRatio.y, 1.0f));
 	}
+
+	IEnumerator checkConnection()
+	{
+		yield return www;
+		
+		if(www.error != null)
+		{
+			print("faild to connect to internet, trying after 2 seconds.");
+			yield return new WaitForSeconds(2);// trying again after 2 sec
+			StartCoroutine(checkConnection());
+		}else
+		{
+			print("connected to internet");
+			Application.LoadLevel("debugscene");
+			
+		}
+		
+	}
 }
+
