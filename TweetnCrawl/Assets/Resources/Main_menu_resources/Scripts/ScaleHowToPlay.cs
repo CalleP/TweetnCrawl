2a7712d4ddceb161af1260760824f47c539f834d
Bergendahl94 <william_bergendahl@hotmail.com>
2014-11-16 18:04:08 +0100

    Added Scrollbar and further design to how to play option

--- TweetnCrawl/Assets/Resources/Main_menu_resources/Scripts/ScaleHowToPlay.cs
+++ TweetnCrawl/Assets/Resources/Main_menu_resources/Scripts/ScaleHowToPlay.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class ScaleHowToPlay : MonoBehaviour 
 {
 	public Texture2D Image;
 	public Texture2D CloseImage;
+	public Texture2D Instructions;
 	public float x;
 	public float y;
 	public float x2;
 	public float y2;
+	public float x3;
+	public float y3;
+	public float x4;
+	public float y4;
 	public GameObject HowTo;
+	public string stringToEdit;
+	public GUIStyle style;
+	public Vector2 scrollPosition = Vector2.zero;
 
 
 	void OnGUI()
@@ -21,7 +30,10 @@ public class ScaleHowToPlay : MonoBehaviour
 		}
 
 		GUI.DrawTexture(new Rect(x, y, Image.width - 50, Image.height - 50), Image);
-
+		scrollPosition = GUI.BeginScrollView(new Rect(x3, y3, CloseImage.width + 345, CloseImage.height + 350), scrollPosition, new Rect(x3, y3, CloseImage.width  + 345 * 2, CloseImage.height  + 350 * 2));
+		GUI.Label(new Rect(x3,y3,CloseImage.width + 345, CloseImage.height + 350), stringToEdit, style);
+		GUI.DrawTexture(new Rect(x4, y4, Instructions.width - 50, Instructions.height - 50), Instructions);
+		GUI.EndScrollView ();
 
 	}
 	
