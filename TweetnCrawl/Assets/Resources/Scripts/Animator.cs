a3e7ff820b8384af706e4c21d6a09bde14fbfa82
Calle Persson <callepersson94@telia.com>
2014-11-20 12:02:07 +0100

    Animation system and some polish

---
+++ TweetnCrawl/Assets/Resources/Scripts/Animator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Animator : MonoBehaviour {
+
+	// Use this for initialization
+
+    public bool loop;
+    public float interval;
+    public List<Sprite> sprites = new List<Sprite>();
+    public bool DeathAfterLastFrame;
+
+    private SpriteRenderer sr;
+    private int index;
+    private float time;
+    void Start () {
+        time = Time.time;
+        index = 0;
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.sprite = sprites[index];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (time <= Time.time)
+        {
+            if ((index >= sprites.Count-1) && loop)
+            {
+                index = 0;
+                sr.sprite = sprites[index];
+
+            }
+            else if (index >= sprites.Count - 1 && DeathAfterLastFrame)
+            {
+                sr.sprite = null;
+            }
+            else if (!(index >= sprites.Count - 1))
+            {
+                index++;
+                sr.sprite = sprites[index];
+            }
+
+            
+            time = Time.time + interval;
+        }
+	}
+}
