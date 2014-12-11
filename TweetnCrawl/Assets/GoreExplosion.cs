5aec111e32766f97e50dbbc4e679defe95b6885b
Calle Persson <callepersson94@telia.com>
2014-12-11 17:57:48 +0100

    Fixed a collision issue

--- TweetnCrawl/Assets/GoreExplosion.cs
+++ TweetnCrawl/Assets/GoreExplosion.cs
@@ -16,7 +16,8 @@ public class GoreExplosion : MonoBehaviour {
             gore.rigidbody2D.AddForce(transform.up * UnityEngine.Random.Range(1000f,2500f));
             gore.transform.rotation = transform.rotation;
             gore.transform.Rotate(new Vector3(0, 0, UnityEngine.Random.Range(0, 360)));
-            //Physics2D.IgnoreCollision(shell.collider2D, player.collider2D);
+
+
         }
 	}
 	
