967811929ba868591981992b502b8a916925c372
Bergendahl94 <william_bergendahl@hotmail.com>
2014-12-13 00:36:28 +0100

    Began Implementing HashtagChoices

--- TweetnCrawl/Assets/Resources/Main_menu_resources/Scripts/PlayGame.cs
+++ TweetnCrawl/Assets/Resources/Main_menu_resources/Scripts/PlayGame.cs
@@ -6,6 +6,8 @@ public class PlayGame : MonoBehaviour
 	public Texture2D Image;
 	public float x;
 	public float y;
+	public float x2;
+	public float y2;
 	WWW www; 
 	public GUITexture guitext;
 	public GUITexture loading;
@@ -13,6 +15,8 @@ public class PlayGame : MonoBehaviour
 	public AudioClip ModemConnect;
 	public AudioClip Onhover;
 	public AudioClip Onclick;
+	private bool Connection = false;
+	protected string Hashtag;
 	
 	void OnGUI()
 	{
@@ -24,8 +28,43 @@ public class PlayGame : MonoBehaviour
 			StartCoroutine(checkConnection());
 
 				}
-		
-		
+
+
+		if (Connection = true) {
+			//TODO move this code to HashtagChoice script instead and use enable/disable script
+			if (GUI.Button (new Rect (x2, y2, 500, 50), "Hashtag1")) {
+				print("Choice1");
+				Hashtag = "1";
+				StartCoroutine(LoadLevel());
+				Connection = false;
+			}
+			if (GUI.Button (new Rect (x2, y2 + 50, 500, 50), "Hashtag2")) {
+				print("Choice2");
+				Hashtag = "2";
+				StartCoroutine(LoadLevel());
+				Connection = false;
+			}
+			if (GUI.Button (new Rect (x2, y2 + 100, 500, 50), "Hashtag3")) {
+				print("Choice3");
+				Hashtag = "3";
+				StartCoroutine(LoadLevel());
+				Connection = false;
+			}
+			if (GUI.Button (new Rect (x2, y2 + 150, 500, 50), "Hashtag4")) {
+				print("Choice4");
+				Hashtag = "4";
+				StartCoroutine(LoadLevel());
+				Connection = false;
+			}
+			if (GUI.Button (new Rect (x2, y2 + 200, 500, 50), "Hashtag5")) {
+				print("Choice5");
+				Hashtag = "5";
+				StartCoroutine(LoadLevel());
+				Connection = false;
+			}
+
+				}
+
 	}
 	
 	public static void AutoResize(int screenWidth, int screenHeight)
@@ -51,13 +90,19 @@ public class PlayGame : MonoBehaviour
 		}else
 		{
 			print("connected to internet");
-			loading.GetComponent<MessageScaling>().enabled = true;
-			audio.PlayOneShot(ModemConnect);
-			yield return new WaitForSeconds(5);
-			Application.LoadLevel("debugscene");
+			Connection = true;
 			
 		}
 		
+	}
+
+	public IEnumerator LoadLevel() {
+
+		loading.GetComponent<MessageScaling>().enabled = true;
+		audio.PlayOneShot(ModemConnect);
+		yield return new WaitForSeconds(5);
+		Application.LoadLevel("debugscene");
+
 	}
 }
 
